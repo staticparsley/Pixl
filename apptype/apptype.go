53c8e8ebfd86c3c8688a584d56db9e785a2e9c0e
staticparsley <ruben.marin@fox.com>
2022-07-24 15:09:59 -0400

    initial setup

---
+++ apptype/apptype.go
@@ -0,0 +1,24 @@
+package apptype
+
+import (
+	"fyne.io/fyne/v2"
+)
+
+type BrushType = int
+type PxCanvasConfig struct {
+	DrawingArea fyne.Size
+	CanvasOffset fyne.Position
+	PxRows, PxCols int
+	PxSize int
+}
+
+type State struct {
+	BrushColor color.Color
+	BrushType int
+	SwatchSelected int
+	FilePath string
+}
+
+func (state *State) setFilePath(path string) {
+	state.FilePath = path
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module zerotomastery.io/pixl
+
+go 1.18
+
+require fyne.io/fyne/v2 v2.2.3
